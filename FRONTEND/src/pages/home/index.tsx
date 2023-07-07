25900f499c746d3d38ce36742dd568f7b92ce198
Jun0zo <joon0zo1022@gmail.com>
2023-07-08 07:58:10 +0900

    intro 추가

--- FRONTEND/src/pages/home/index.tsx
+++ FRONTEND/src/pages/home/index.tsx
@@ -1,22 +1,13 @@
 // ** MUI Imports
-import Card from "@mui/material/Card";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import CardHeader from "@mui/material/CardHeader";
-import CardContent from "@mui/material/CardContent";
+
 import MenuList from "src/views/pages/home/MenuList";
+import Intro from "src/views/pages/home/Intro";
 
 const Home = () => {
   return (
-    <div
-      style={{
-        backgroundColor: "red",
-      }}
-    >
-      <div style={{ padding: "50px 0px" }}>
-        <h1>DOGGY DOGGY</h1>
-        <Typography>하지나</Typography>
-      </div>
+    <div style={{}}>
+      <Intro />
       <MenuList />
     </div>
   );
