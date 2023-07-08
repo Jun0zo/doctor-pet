8d4a7f55f433e0a099bb2d1426d58ecaa297a0f0
Jun0zo <joon0zo1022@gmail.com>
2023-07-08 10:23:06 +0900

    page initialize (#4)

---
+++ FRONTEND/src/views/pages/hospitals/HospitalMap.tsx
@@ -0,0 +1,8 @@
+// ** MUI Imports
+import Typography from "@mui/material/Typography";
+
+const HospitalMap = () => {
+  return <div style={{}}>{/* <Intro /> */}</div>;
+};
+
+export default HospitalMap;
