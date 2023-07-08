6bc215f7092fa07e0cb4ab7adca7058cec05ba31
Jun0zo <joon0zo1022@gmail.com>
2023-07-08 09:32:04 +0900

    card에 설명 추가

--- FRONTEND/src/views/pages/home/MenuList.tsx
+++ FRONTEND/src/views/pages/home/MenuList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 // ** MUI Imports
-import { Card, Grid, CardHeader, CardContent } from "@mui/material";
+import { Card, Grid, CardHeader, CardContent, Box } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import DogImage from "src/images/dog.png";
 import DogCallendar from "src/images/dog_callendar.png";
@@ -24,30 +24,90 @@ const MenuList = () => {
   }, []);
 
   const menuInfos = [
-    { title: "애완견 진단", img: DogImage },
-    { title: "예약 일정", img: DogCallendar },
-    { title: "주변 병원", img: DogHospital },
-    { title: "주변 약국", img: Pharmacy },
+    {
+      title: "애완견 진단",
+      img: DogImage,
+      describe: "자신의 애완견을 AI를 통해 진단해보세요",
+    },
+    {
+      title: "예약 일정",
+      img: DogCallendar,
+      describe: "애완견의 예약 일정을 확인해보세요",
+    },
+    {
+      title: "주변 병원",
+      img: DogHospital,
+      describe: "주변에 어떤 병원이 있는지 알아볼까요?",
+    },
+    {
+      title: "주변 약국",
+      img: Pharmacy,
+      describe: "주변에 어떤 약국이 있는지 알아볼까요?",
+    },
   ];
 
   console.log(menuInfos);
 
   return (
-    <Grid container spacing={6}>
+    <Grid container spacing={10}>
       {menuInfos.map((menuInfo) => (
         <Grid item md={3} sm={6} xs={12}>
           {isXs ? (
-            <Card sx={{ textAlign: "center" }}>
+            <Card
+              sx={{
+                textAlign: "center",
+                borderRadius: "30px",
+                transition: "transform 0.3s",
+                cursor: "pointer",
+                "&:hover": {
+                  transform: "scale(1.1)",
+                },
+              }}
+            >
               <CardContent sx={{ padding: "1.25rm", display: "flex" }}>
-                <img src={menuInfo.img.src} width="100px" />
-                <h3>{menuInfo.title}</h3>
+                <Box>
+                  <img src={menuInfo.img.src} width="100px" />
+                </Box>
+                <Box
+                  sx={{
+                    textAlign: "center",
+                    padding: "0px 2rem",
+                  }}
+                >
+                  <h3>{menuInfo.title}</h3>
+                  <Typography sx={{ textAlign: "left" }}>
+                    {menuInfo.describe}
+                  </Typography>
+                </Box>
               </CardContent>
             </Card>
           ) : (
-            <Card sx={{ textAlign: "center" }}>
-              <CardContent sx={{ textAlign: "center" }}>
-                <h3>{menuInfo.title}</h3>
-                <img src={menuInfo.img.src} width="200px" />
+            <Card
+              sx={{
+                textAlign: "center",
+                borderRadius: "2rem",
+                transition: "transform 0.3s",
+                cursor: "pointer",
+                "&:hover": {
+                  transform: "scale(1.1)",
+                },
+              }}
+            >
+              <CardContent>
+                <Box>
+                  <img src={menuInfo.img.src} width="150px" />
+                </Box>
+                <Box
+                  sx={{
+                    textAlign: "center",
+                    padding: "0px 2rem",
+                  }}
+                >
+                  <h3>{menuInfo.title}</h3>
+                  <Typography sx={{ textAlign: "left" }}>
+                    {menuInfo.describe}
+                  </Typography>
+                </Box>
               </CardContent>
             </Card>
           )}
