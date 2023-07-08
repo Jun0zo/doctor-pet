8e083549250071e937f7bae4989ee24dd1906445
Jun0zo <joon0zo1022@gmail.com>
2023-07-08 10:53:26 +0900

    페이지에 지도 표시 (#4)

--- FRONTEND/src/pages/hospitals/index.tsx
+++ FRONTEND/src/pages/hospitals/index.tsx
@@ -1,9 +1,11 @@
-// ** MUI Imports
-import Typography from "@mui/material/Typography";
+import React, { useEffect, useState } from "react";
 
+// ** View Pages Imports
 import HospitalMap from "src/views/pages/hospitals/HospitalMap";
 
 const Hospitals = () => {
+  //   let kakao = window["kakao"];
+
   return (
     <div style={{}}>
       {/* <Intro /> */}
--- FRONTEND/src/views/pages/hospitals/HospitalMap.tsx
+++ FRONTEND/src/views/pages/hospitals/HospitalMap.tsx
@@ -1,8 +1,21 @@
 // ** MUI Imports
 import Typography from "@mui/material/Typography";
 
+// ** Third Party Imports
+import { Map } from "react-kakao-maps-sdk";
+
 const HospitalMap = () => {
-  return <div style={{}}>{/* <Intro /> */}</div>;
+  return (
+    <div style={{}}>
+      {
+        <Map
+          center={{ lat: 33.5563, lng: 126.79581 }} // 지도의 중심 좌표
+          style={{ width: "800px", height: "600px" }} // 지도 크기
+          level={3}
+        />
+      }
+    </div>
+  );
 };
 
 export default HospitalMap;
