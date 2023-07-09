620e53c5d51520490d55a996682677dff066b752
Jun0zo <joon0zo1022@gmail.com>
2023-07-09 18:31:39 +0900

    introduce 부분 변경 (#5)

--- FRONTEND/src/views/pages/home/Intro.tsx
+++ FRONTEND/src/views/pages/home/Intro.tsx
@@ -1,29 +1,54 @@
 import React, { useEffect, useState } from "react";
 
 // ** MUI Imports
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import { useMediaQuery } from "react-responsive";
+
+import DogImage from "src/images/dog_image.png";
 
 const Intro = () => {
-  const [isXs, setIsXs] = useState(window.innerWidth < 600);
+  const [width, setWidth] = useState("80%");
+  const isXs = useMediaQuery({ maxWidth: 600 });
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsXs(window.innerWidth < 600);
-    };
+  const isDesktop: boolean = useMediaQuery({
+    query: "(min-width:1024px)",
+  });
 
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
   return (
     <>
       {isXs ? null : (
-        <div style={{ padding: "50px 0px" }}>
-          <h1>OKKY DOGGY</h1>
-          <Typography>하지나</Typography>
-        </div>
+        <Box
+          sx={{
+            backgroundColor: "#b2feb2",
+            height: "400px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            position: "relative",
+          }}
+        >
+          <Box
+            sx={{
+              padding: "100px 0px",
+              maxWidth: "1200px",
+              width: "100%",
+              margin: "0 auto",
+            }}
+          >
+            <Box>
+              <h1>OKKY DOGGY</h1>
+              <Typography>Hannah</Typography>
+            </Box>
+          </Box>
+
+          <Box sx={{ position: "absolute", bottom: "0", right: "0" }}>
+            <img
+              src={DogImage.src}
+              style={{ transform: "scaleX(-1)", height: "400px", margin: "0" }}
+            />
+          </Box>
+        </Box>
       )}
     </>
   );
