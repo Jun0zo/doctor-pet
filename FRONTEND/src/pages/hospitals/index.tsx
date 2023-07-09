a3e3167fc637aae16fa1e408d5961bfe67a8d2e3
Jun0zo <joon0zo1022@gmail.com>
2023-07-09 09:22:27 +0900

    지도, table 추가 (#4)

--- FRONTEND/src/pages/hospitals/index.tsx
+++ FRONTEND/src/pages/hospitals/index.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 
+import { Grid } from "@mui/material";
+
 // ** View Pages Imports
 import HospitalMap from "src/views/pages/hospitals/HospitalMap";
+import HospitalTable from "src/views/pages/hospitals/HospitalTable";
 
 const Hospitals = () => {
   //   let kakao = window["kakao"];
@@ -9,7 +12,14 @@ const Hospitals = () => {
   return (
     <div style={{}}>
       {/* <Intro /> */}
-      <HospitalMap />
+      <Grid container>
+        <Grid item xs={12} lg={6} sx={{ height: "200px" }}>
+          <HospitalMap />
+        </Grid>
+        <Grid item xs={12} lg={6} sx={{ height: "500px" }}>
+          <HospitalTable />
+        </Grid>
+      </Grid>
     </div>
   );
 };
